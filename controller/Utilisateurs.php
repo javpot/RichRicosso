c41cdb1b627e395b1074dcee9f5f2c813172ef43
GajdaRichard <115659563+GajdaRichard@users.noreply.github.com>
2023-11-04 02:00:01 -0400

    Added models

---
+++ controller/Utilisateurs.php
@@ -0,0 +1,34 @@
+<?php
+require_once 'model/Utilisateurs.php';
+class UtilisateursController
+{
+    private $model;
+    public function __construct($pdo)
+    {
+        $this->model = new UtilisateursModel($pdo);
+    }
+    public function getAllUsers()
+    {
+        return $this->model->getAllUsers();
+    }
+    public function getUserById($id)
+    {
+        return $this->model->getUserById($id);
+    }
+    public function createUser($data)
+    {
+        $data['mot_de_passe'] = password_hash(
+            $data['mot_de_passe'],
+            PASSWORD_DEFAULT
+        );
+        return $this->model->createUser($data);
+    }
+    public function updateUser($id, $data)
+    {
+        return $this->model->updateUser($id, $data);
+    }
+    public function deleteUser($id)
+    {
+        return $this->model->deleteUser($id);
+    }
+}
