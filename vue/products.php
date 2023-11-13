3284bce78f3c0134d770d994fcec3821b291d968
GajdaRichard <115659563+GajdaRichard@users.noreply.github.com>
2023-11-13 13:07:21 -0500

    Filters:Done

--- vue/products.php
+++ vue/products.php
@@ -3,12 +3,15 @@ require_once("../manager/SessionManager.php");
 require_once("../manager/DatabaseManager.php");
 require_once("../controller/Utilisateurs.php");
 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 $session = SessionManager::getInstance();
 $db = DBManager::getInstance();
 
 $produitsController = $db->getControllerProduct();
 
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['filters'])) {
     $typeFilter = isset($_POST['type']) ? $_POST['type'] : null;
     $couleurFilter = isset($_POST['couleur']) ? $_POST['couleur'] : null;
     $tailleFilter = isset($_POST['taille']) ? $_POST['taille'] : null;
@@ -42,6 +45,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
         <p class="x-text" id="x-text">X</p>
       </div>
       <form action="" method="post">
+        <input type="hidden" name="filters">
         <div class="type">
           <p>Type:</p>
           <label for="cravatte-check">Cravatte</label>
@@ -52,32 +56,32 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
         <div class="couleur">
           <p>Couleur:</p>
           <label for="mauve-check">Mauve</label>
-          <input type="checkbox" name="couleur" id="mauve-check" />
+          <input type="checkbox" name="couleur" id="mauve-check" value="Mauve" />
           <label for="rose-check">Rose</label>
-          <input type="checkbox" name="couleur" id="rose-check" />
+          <input type="checkbox" name="couleur" id="rose-check" value="Rose" />
           <label for="bleu-check">Bleu</label>
-          <input type="checkbox" name="couleur" id="bleu-check" />
+          <input type="checkbox" name="couleur" id="bleu-check" value="Bleu" />
           <label for="noir-check">Noir</label>
-          <input type="checkbox" name="couleur" id="noir-check" />
+          <input type="checkbox" name="couleur" id="noir-check" value="Noir" />
           <label for="vert-check">Vert</label>
-          <input type="checkbox" name="couleur" id="vert-check" />
+          <input type="checkbox" name="couleur" id="vert-check" value="Vert" />
         </div>
         <div class="taille">
           <p>Taille de chemise:</p>
           <label for="taille-44">44</label>
-          <input class="taille" type="checkbox" name="taille" id="taille-44" />
+          <input class="taille" type="checkbox" name="taille" id="taille-44" value="44" />
           <label for="taille-46">46</label>
-          <input class="taille" type="checkbox" name="taille" id="taille-46" />
+          <input class="taille" type="checkbox" name="taille" id="taille-46" value="46" />
           <label for="taille-48">48</label>
-          <input class="taille" type="checkbox" name="taille" id="taille-48" />
+          <input class="taille" type="checkbox" name="taille" id="taille-48" value="48" />
           <label for="taille-50">50</label>
-          <input class="taille" type="checkbox" name="taille" id="taille-50" />
+          <input class="taille" type="checkbox" name="taille" id="taille-50" value="50" />
           <label for="taille-52">52</label>
-          <input class="taille" type="checkbox" name="taille" id="taille-52" />
+          <input class="taille" type="checkbox" name="taille" id="taille-52" value="52" />
           <label for="taille-54">54</label>
-          <input class="taille" type="checkbox" name="taille" id="taille-54" />
+          <input class="taille" type="checkbox" name="taille" id="taille-54" value="54" />
           <label for="taille-56">56</label>
-          <input class="taille" type="checkbox" name="taille" id="taille-56" />
+          <input class="taille" type="checkbox" name="taille" id="taille-56" value="56" />
         </div>
         <div class="range">
           <label for="range-prix">Range de prix:</label>
