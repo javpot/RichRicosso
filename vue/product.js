b10f86eef3435e7135f7a9480495766a2f3a8cbb
GajdaRichard <115659563+GajdaRichard@users.noreply.github.com>
2023-11-13 21:37:46 -0500

    2 image product

--- vue/product.js
+++ vue/product.js
@@ -7,13 +7,10 @@ log = document.getElementById("log");
 const imgElement = document.getElementById("image-product");
 dot1 = document.getElementById("dot1");
 dot2 = document.getElementById("dot2");
-let dot1Selected = true;
+dot1Selected = true;
 
-
-// Obtenez la valeur de l'attribut "src" de l'élément img
 const imgSrc = imgElement.getAttribute("src");
 
-// Stockez la valeur de "src" dans la variable imgProduct
 const imgProduct = imgSrc;
 
 console.log(imgProduct);
@@ -31,49 +28,34 @@ const hideSidebar = () => {
 };
 
 const dot1Clicked = () => {
-
-    if (dot1Selected == true) {
-        dot1.style.pointerEvents = "none";
-
-    } else {
+    if (dot1Selected == false) {
         dot1.style.backgroundColor = "#404040";
         dot2.style.backgroundColor = "#6f6f6f";
-        const currentSrc = imgElement.getAttribute("src");
-        const lastDotIndex = currentSrc.lastIndexOf(".");
-        console.log(currentSrc);
-        const beforeLastDot = currentSrc.substring(0, lastDotIndex);
-        const afterLastDot = currentSrc.substring(lastDotIndex);
-
+        currentSrc = imgElement.getAttribute("src");
+        lastDotIndex = currentSrc.lastIndexOf(".");
+        beforeLastDot = currentSrc.substring(0, lastDotIndex);
+        afterLastDot = currentSrc.substring(lastDotIndex);
         if (beforeLastDot.charAt(beforeLastDot.length - 1) === "1") {
             beforeLastDot = beforeLastDot.substring(0, beforeLastDot.length - 1);
         }
-        const newSrc = beforeLastDot + afterLastDot;
+        newSrc = beforeLastDot + afterLastDot;
         imgElement.setAttribute("src", newSrc);
         dot1Selected = true;
     }
 
 }
 const dot2Clicked = () => {
-
-
-    if (dot1Selected == false) {
-        dot2.style.pointerEvents = "none";
-    }
-    else {
-
+    if (dot1Selected) {
         dot2.style.backgroundColor = "#404040";
         dot1.style.backgroundColor = "#6f6f6f";
         const currentSrc = imgElement.getAttribute("src");
-        console.log(currentSrc);
         const lastDotIndex = currentSrc.lastIndexOf(".");
         const beforeLastDot = currentSrc.substring(0, lastDotIndex);
         const afterLastDot = currentSrc.substring(lastDotIndex);
         const newSrc = beforeLastDot + "1" + afterLastDot;
         imgElement.setAttribute("src", newSrc);
-        dot1Selected == true;
+        dot1Selected = false;
     }
-
-
 }
 
 menu.addEventListener("click", showSidebar);
